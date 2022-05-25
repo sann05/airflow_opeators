1f3fe7325f138643cb57c9ea24e6f2cd83e3100c
Aliaksandr Sheliutsin <aliaksandr.sheliutsin@coinspaid.com>
2022-05-25 13:35:53 +0400

    Initial commit:  - Added README  - Added external dagrun sensors

---
+++ README.md
@@ -0,0 +1,13 @@
+# Airflow operators
+
+In my day-to-day work, I often feel a need for some Airflow operators, so I decided to create some of them and here they are.
+
+## Installation and Usage
+
+No installation is needed, just copy the code in your project and enjoy
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ sensor/external_dagrun.py
@@ -0,0 +1,77 @@
+from datetime import datetime, timedelta
+from typing import Dict, Any
+
+from airflow import AirflowException
+from airflow.models import DagRun, DagBag
+from airflow.sensors.base import BaseSensorOperator
+from airflow.sensors.external_task import ExternalTaskSensor
+from airflow.utils.db import provide_session
+from airflow.utils.state import State
+from croniter import croniter
+
+
+class LastExternalDagRunStatusSensor(BaseSensorOperator):
+    """
+    The sensor checks that status of the last started DagRun in allowed states
+    """
+    template_fields = ['external_dag_id']
+    ui_color = '#19647e'
+
+    def __init__(self,
+                 external_dag_id,
+                 allowed_states=None,
+                 check_existence=False,
+                 *args,
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.allowed_states = allowed_states or [State.SUCCESS]
+
+        self.external_dag_id = external_dag_id
+        self.check_existence = check_existence
+
+    @provide_session
+    def poke(self, context, session=None):
+        self.log.info(
+            'Poking for %s states in %s ... ',
+            self.external_dag_id,
+            str(self.allowed_states)
+        )
+
+        DR = DagRun
+        last_dag_run = session.query(DR).filter(
+            DR.dag_id == self.external_dag_id
+        ).order_by(DR.execution_date.desc()).first()
+
+        return last_dag_run.state in self.allowed_states
+
+
+class ClosestExternalTaskSensor(ExternalTaskSensor):
+    """
+    Sensor finds closes external execution date before the sensor execution date
+    """
+
+    def _get_external_latest_execution_date(self, execution_date):
+        external_dag = DagBag().get_dag(self.external_dag_id)
+        if not isinstance(external_dag.schedule_interval, str):
+            raise AirflowException(
+                "Only possible to work with str schedule_interval, not "
+                f"{type(external_dag.schedule_interval)} for"
+                f" {external_dag.schedule_interval}"
+            )
+        itr = croniter(external_dag.schedule_interval, execution_date)
+        dt = itr.get_prev(datetime)
+        self.log.info(f"The latest execution_date for {self.external_dag_id} is {dt}")
+        return dt
+
+    def __init__(self, *args, **kwargs) -> None:
+        # Mock value of execution_delta to avoid errors during initialization
+        # of the sensor
+        kwargs["execution_delta"] = timedelta(days=1)
+        super().__init__(*args, **kwargs)
+
+
+
+    def execute(self, context: Dict) -> Any:
+        execution_date = context["execution_date"]
+        self.execution_delta = execution_date - self._get_external_latest_execution_date(execution_date)
+        super().execute(context)
